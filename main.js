5f436bd78b6f1791b17a1165d1d77b8ba38c24e1
Manuel <manuel.devito@icloud.com>
2020-10-14 15:30:23 +0200

    Consegna esercizio

--- main.js
+++ main.js
@@ -17,19 +17,19 @@ var prezzo = chilometri_da_percorrere * prezzo_al_chilometro;
 
 // utente minorenne sconto 20%.
 
-var sconto_minorenne = 0.2;
+var sconto_minorenne = 20;
 
 // utente over65 sconto 40%;
 
-var sconto_over65 = 0.4;
+var sconto_over65 = 40;
 
 // calcolo prezzo biglietto minorenne.
 
-var prezzo_minorenne = prezzo - (prezzo * sconto_minorenne);
+var prezzo_minorenne = prezzo - (prezzo * sconto_minorenne / 100);
 
 // calcolo prezzo biglietto over65.
 
-var prezzo_over65 = prezzo - (prezzo * sconto_over65);
+var prezzo_over65 = prezzo - (prezzo * sconto_over65 / 100);
 
 
 
@@ -37,4 +37,6 @@ if (eta_utente < 18) {
     document.getElementById('biglietto').innerHTML = ('Prezzo Biglietto: ' + prezzo_minorenne + '€');
 } else if (eta_utente > 65) {
     document.getElementById('biglietto').innerHTML = ('Prezzo Biglietto: ' + prezzo_over65 + '€');
+} else {
+    document.getElementById('biglietto').innerHTML = ('Il prezzo del biglietto è: ' + prezzo + '€');
 }
